15e050e1130ce9378553213283255de03ee4a28c
Thilina-Pathirage <thilinapathirage@gmail.com>
2021-10-01 21:11:50 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sample from './components/Sample'; 
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Sample/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sample.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Sample.css';
+
+export default function Sample() {
+    return (
+        <div>
+            <h1 className="my-h1">Hello World</h1>
+        </div>
+    )
+}
