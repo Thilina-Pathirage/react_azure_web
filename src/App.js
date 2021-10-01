093f2ed8e8b562da8182b91214717cbef9f22170
hirunagrad <hirunagrad@gmail.com>
2021-10-01 22:23:41 +0530

    fix come probs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Sample from './components/Sample'; 
 import Navbar from './components/Navbar';
+import Gay from './components/pathiraGAY';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Sample/>
+      <Gay/>
     </div>
   );
 }
