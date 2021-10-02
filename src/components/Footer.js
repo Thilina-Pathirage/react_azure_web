11339b4767368a42d2732d01eb9ba45b203e7896
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-10-02 13:36:01 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdb-react-ui-kit';
+
 
 export default function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <>
+            <MDBFooter backgroundColor='light' className='text-center text-lg-left'>
+                <MDBContainer className='p-4 pb-0'>
+                    <form action=''>
+                    <MDBRow>
+                        <MDBCol size='auto' className='mb-4 mb-md-0'>
+                        <p className='pt-2'>
+                            <strong>Sign up for our newsletter</strong>
+                        </p>
+                        </MDBCol>
+
+                        <MDBCol md='5' size='12' className='mb-4 mb-md-0'>
+                        <MDBInput type='text' id='form5Example2' label='Email address' />
+                        </MDBCol>
+
+                        <MDBCol size='auto' className='mb-4 mb-md-0'>
+                        <MDBBtn>Subscribe</MDBBtn>
+                        </MDBCol>
+                    </MDBRow>
+                    </form>
+                </MDBContainer>
+
+                <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                    &copy; {new Date().getFullYear()} Copyright:{' '}
+                    <a className='text-dark' href='https://mdbootstrap.com/'>
+                    MDBootstrap.com
+                    </a>
+                </div>
+            </MDBFooter>
+        </>
     )
 }
